ad7006660e50aa497b1152d210355aae6a7681ba
Victor Hernandez <vichdz97@txstate.edu>
2023-05-03 12:18:48 -0500

    added skills to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  skills = [
+    { fileName: "c", name: "C Programming"}, 
+    { fileName: "c++", name: "C++"}, 
+    { fileName: "java", name: "Java"}, 
+    { fileName: "spring-boot", name: "Spring Boot"}, 
+    { fileName: "html", name: "HTML"}, 
+    { fileName: "css", name: "CSS"}, 
+    { fileName: "javascript", name: "JavaScript"}, 
+    { fileName: "bootstrap", name: "Bootstrap"}, 
+    { fileName: "python", name: "Python"}, 
+    { fileName: "react", name: "React"}, 
+    { fileName: "angular", name: "Angular"}, 
+    { fileName: "typescript", name: "TypeScript"}, 
+    { fileName: "postman", name: "Postman"}, 
+    { fileName: "git", name: "Git"}, 
+    { fileName: "vs", name: "VS Code"}, 
+    { fileName: "intellij", name: "IntelliJ IDEA"}, 
+    { fileName: "android-studio", name: "Android Studio"}
+  ];
+
 }
