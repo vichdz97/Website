118c7d53a48eac099e7bf7919839e39f3fd7aa6d
Victor Hernandez <vichdz97@txstate.edu>
2023-05-17 17:26:40 -0500

    removed azure from tech stack

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -32,7 +32,7 @@ export class HomeComponent {
     { 
       title: "Authentication System", 
       img: "authsys.png",
-      tech: ["angular", "bootstrap", "java", "spring-boot", "azure"],
+      tech: ["angular", "bootstrap", "java", "spring-boot"],
       description: `Designed a responsive web application that redirects the user 
                     to the appropriate landing page given specific login credentials.`,
       url: "https://github.com/vichdz97/AuthenticationSystem"
