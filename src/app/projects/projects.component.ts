61bb70e91ccc4e8277613e92d63d92ef0e097371
Victor Hernandez <vichdz97@txstate.edu>
2023-05-17 17:26:04 -0500

    updated projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
+  projects = [
+    { 
+      title: "Authentication System", 
+      img: "authsys.png",
+      tech: ["angular", "bootstrap", "java", "spring-boot"],
+      description: `Designed a responsive web application that redirects the user 
+                    to the appropriate landing page given specific login credentials.`,
+      url: "https://github.com/vichdz97/AuthenticationSystem"
+    }, 
+    { 
+      title: "Library Management System",
+      img: "library.png",
+      tech: ["angular", "bootstrap", "java", "spring-boot"],
+      description: `Created a dynamic web application that allows the user to 
+                    manage books in a virtual library environment`,
+      url: "https://github.com/vichdz97/Library-Management-System"
+    }
+  ];
+
 }
