f6acd0fd9ab33d3eb04d8ee91259a1df46a1bc43
Victor Hernandez <vichdz97@txstate.edu>
2023-05-23 17:28:26 -0500

    added bubbles project

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -14,7 +14,7 @@ export class ProjectsComponent {
       img: "authsys.png",
       tech: ["angular", "bootstrap", "java", "spring-boot"],
       description: `Designed a responsive web application that redirects the user 
-                    to the appropriate landing page given specific login credentials.`,
+                    to the appropriate landing page given specific login credentials`,
       url: "https://github.com/vichdz97/AuthenticationSystem"
     }, 
     { 
@@ -24,6 +24,15 @@ export class ProjectsComponent {
       description: `Created a dynamic web application that allows the user to 
                     manage books in a virtual library environment`,
       url: "https://github.com/vichdz97/Library-Management-System"
+    },
+    {
+      title: "Bubbles Demo",
+      img: "bubbles.png",
+      tech: ["html", "css", "javascript"],
+      description: `Coded an interactive web application that allows the user
+                    to make music at the click of a button`,
+      url: "https://github.com/vichdz97/bubbles",
+      web: "https://bubbles-demo.victor-jr.com"
     }
   ];
 
