447bd89ff178251db0de42bf2326ef57b335fc25
Victor Hernandez <vichdz97@txstate.edu>
2023-05-24 03:09:57 -0500

    created a responsive navbar

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+
+  isCollapsed: boolean = true;
+
+  constructor(private router: Router) {}
+
+  redirectTo(webpage: string) {
+    this.isCollapsed = true;
+    this.router.navigateByUrl(webpage);
+  }
 
 }
