1280d01184dc56597e591a1731787a85bb36b333
Victor Hernandez <vichdz97@txstate.edu>
2023-06-02 15:48:06 -0500

    added calculator project

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -13,26 +13,35 @@ export class ProjectsComponent {
       title: "Authentication System", 
       img: "authsys.png",
       tech: ["angular", "bootstrap", "java", "spring-boot"],
-      description: `Designed a responsive web application that redirects the user 
-                    to the appropriate landing page given specific login credentials`,
+      description: `A responsive web application that redirects the user 
+                    to the appropriate landing page given specific login credentials.`,
       url: "https://github.com/vichdz97/AuthenticationSystem"
     }, 
     { 
       title: "Library Management System",
       img: "library.png",
       tech: ["angular", "bootstrap", "java", "spring-boot"],
-      description: `Created a dynamic web application that allows the user to 
-                    manage books in a virtual library environment`,
+      description: `A dynamic web application that allows the user to 
+                    manage books in a virtual library environment.`,
       url: "https://github.com/vichdz97/Library-Management-System"
     },
     {
       title: "Bubbles Demo",
       img: "bubbles.png",
       tech: ["html", "css", "javascript"],
-      description: `Coded an interactive web application that allows the user
-                    to make music at the click of a button`,
+      description: `An interactive web application that allows the user
+                    to make music at the click of a button.`,
       url: "https://github.com/vichdz97/bubbles",
       web: "https://bubbles-demo.victor-jr.com"
+    },
+    {
+      title: "Calculator App",
+      img: "calculator.png",
+      tech: ["react", "typescript", "bootstrap"],
+      description: `A simple calculator app that performs various
+                    mathematical operations.`,
+      url: "https://github.com/vichdz97/react-calculator",
+      web: "https://calculator-app.victor-jr.com"
     }
   ];
 
