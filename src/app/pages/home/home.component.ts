106c4a942ca0b7d967bf9bb180f001da4aa5193c
Victor Hernandez, Jr <vh4697@gmail.com>
2024-07-31 21:30:35 -0500

    hiding auth sys project since in-progress

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -40,15 +40,15 @@ export class HomeComponent {
     ];
 
     projects = [
-        { 
-            title: "Authentication System", 
-            img: "authsys-progress.png",
-            tech: ["aws", "mysql", "java", "spring-boot", "angular", "bootstrap", "postman"],
-            description: `A responsive web application that redirects the user 
-                        to the appropriate landing page given specific login credentials.`,
-            // url: "http://authentication-system.s3-website.us-east-2.amazonaws.com/#/login",
-            github: "https://github.com/vichdz97/Authentication-System"
-        },
+        {
+            title: "Pokémon Generator",
+            img: "pokemon.png",
+            tech: ["pokeapi", "react", "typescript", "bootstrap"],
+            description: `A random Pokémon generator that displays a Pokémon's sprite 
+                          and game info.`,
+            url: "https://pokemon-generator.victor-jr.com",
+            github: "https://github.com/vichdz97/pokemon-generator"
+          },
         {
             title: "Calculator App",
             img: "calculator.png",
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -9,15 +9,6 @@ import { Title } from '@angular/platform-browser';
 export class ProjectsComponent {
 
   projects = [
-    { 
-      title: "Authentication System", 
-      img: "authsys-progress.png",
-      tech: ["aws", "mysql", "java", "spring-boot", "angular", "bootstrap", "postman"],
-      description: `A responsive web application that redirects the user 
-                    to the appropriate landing page given specific login credentials.`,
-      // url: "http://authentication-system.s3-website.us-east-2.amazonaws.com/#/login",
-      github: "https://github.com/vichdz97/Authentication-System"
-    },
     {
       title: "Pokémon Generator",
       img: "pokemon.png",
@@ -35,6 +26,15 @@ export class ProjectsComponent {
                     mathematical operations.`,
       url: "https://calculator-app.victor-jr.com",
       github: "https://github.com/vichdz97/react-calculator"
+    },
+    { 
+      title: "Authentication System", 
+      img: "authsys-progress.png",
+      tech: ["aws", "mysql", "java", "spring-boot", "angular", "bootstrap", "postman"],
+      description: `A responsive web application that redirects the user 
+                    to the appropriate landing page given specific login credentials.`,
+      // url: "http://authentication-system.s3-website.us-east-2.amazonaws.com/#/login",
+      github: "https://github.com/vichdz97/Authentication-System"
     }
   ];
 
