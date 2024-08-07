d52e2b867ca6786c6ea30af5552389b498807b2c
Victor Hernandez, Jr <vh4697@gmail.com>
2024-08-06 21:56:59 -0500

    added flip-n-match to arcade

--- src/app/pages/arcade/arcade.component.ts
+++ src/app/pages/arcade/arcade.component.ts
@@ -10,6 +10,22 @@ export class ArcadeComponent {
 
   games = [
     {
+      title: "Tic-Tac-Toe",
+      img: "tic-tac-toe.png",
+      tech: ["react", "typescript", "bootstrap"],
+      description: `A fun, competitive 2-Player game of Tic-Tac-Toe.`,
+      url: "https://tic-tac-toe.victor-jr.com",
+      github: "https://github.com/vichdz97/tic-tac-toe"
+    },
+    {
+      title: "Flip 'N Match",
+      img: "flip-n-match.png",
+      tech: ["html", "css", "javascript"],
+      description: `Try and beat the clock in this interactive card matching game!`,
+      url: "https://flip-n-match.victor-jr.com",
+      github: "https://github.com/vichdz97/flip-n-match"
+    },
+    {
       title: "Piano Tap",
       img: "piano-tap.png",
       tech: ["html", "css", "javascript"],
@@ -17,14 +33,6 @@ export class ArcadeComponent {
                     to make music with the tap of a tile.`,
       url: "https://piano-tap.victor-jr.com",
       github: "https://github.com/vichdz97/Piano-Tap"
-    },
-    {
-      title: "Tic-Tac-Toe",
-      img: "tic-tac-toe.png",
-      tech: ["react", "typescript", "bootstrap"],
-      description: `A fun, competitive 2-Player game of Tic-Tac-Toe.`,
-      url: "https://tic-tac-toe.victor-jr.com",
-      github: "https://github.com/vichdz97/tic-tac-toe"
     },
   ];
 
