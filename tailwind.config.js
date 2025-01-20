39bfb97bd050eb0d9d24c9a1fe73f4418f7726db
Victor Hernandez, Jr <vh4697@gmail.com>
2025-01-19 23:48:07 -0600

    added custom color 'dark-blue'

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': '#010020'
+      }
+    },
   },
   plugins: [],
 }
