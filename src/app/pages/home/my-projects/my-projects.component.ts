3e740f51012e15c89e9bc88d10f313ebe9a94a11
Victor Hernandez, Jr <vh4697@gmail.com>
2025-01-24 16:17:41 -0600

    replaced pokemon generator with weather app

--- src/app/pages/home/my-projects/my-projects.component.ts
+++ src/app/pages/home/my-projects/my-projects.component.ts
@@ -9,23 +9,23 @@ export class MyProjectsComponent {
 
   projects = [
     { 
-        title: "Authentication System", 
-        img: "authsys.png",
-        tech: ["amazonwebservices", "angular", "bootstrap", "spring", "mysql", "postman"],
-        description: `A dynamic web app that redirects users to the 
-                      correct landing page based on their login credentials.`,
-        url: "http://authentication-system.s3-website.us-east-2.amazonaws.com",
-        github: "https://github.com/vichdz97/Authentication-System"
+      title: "Authentication System", 
+      img: "authsys.png",
+      tech: ["amazonwebservices", "angular", "bootstrap", "spring", "mysql", "postman"],
+      description: `A dynamic web app that redirects users to the 
+                    correct landing page based on their login credentials.`,
+      url: "http://authentication-system.s3-website.us-east-2.amazonaws.com",
+      github: "https://github.com/vichdz97/Authentication-System"
     },
     {
-        title: "Pokémon Generator",
-        img: "pokemon.png",
-        tech: ["pokeapi", "react", "typescript", "bootstrap"],
-        description: `Unleash any Pokémon with this random generator 
-                    that reveals its sprite and game stats.`,
-        url: "https://pokemon-generator.victor-jr.com",
-        github: "https://github.com/vichdz97/pokemon-generator"
-    }
+      title: "Weather App",
+      img: "weather.png",
+      tech: ["openweatherapi", "react", "typescript", "tailwindcss"],
+      description: `An intuitive weather app inspired by Apple's weather interface, 
+                    providing real-time forecasts and detailed weather information.`,
+      url: "https://weather-app.victor-jr.com",
+      github: "https://github.com/vichdz97/weather-app"
+    },
   ];
 
 }
