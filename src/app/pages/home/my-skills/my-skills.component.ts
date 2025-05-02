c9ec3b4333970ac1a8c29e4d3b7766d502613dee
Victor Hernandez, Jr <vh4697@gmail.com>
2025-05-02 16:38:20 -0500

    organized skills array by grouping similar skills and sorted them alphabetically "OnInit"

--- src/app/pages/home/my-skills/my-skills.component.ts
+++ src/app/pages/home/my-skills/my-skills.component.ts
@@ -1,41 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Skills {
+  fileName: string;
+  name: string;
+};
 
 @Component({
   selector: 'app-my-skills',
   templateUrl: './my-skills.component.html',
   styleUrls: ['./my-skills.component.css']
 })
-export class MySkillsComponent {
+export class MySkillsComponent implements OnInit {
 
   // svgs found here -> https://devicon.dev
-  skills = [
-    { fileName: "amazonwebservices", name: "AWS"},
-    { fileName: "androidstudio", name: "Android Studio"},
-    { fileName: "angular", name: "Angular"},
-    { fileName: "bootstrap", name: "Bootstrap"},
-    { fileName: "c", name: "C"},
-    { fileName: "cplusplus", name: "C++"},
-    { fileName: "csharp", name: "C#"},
-    { fileName: "clion", name: "CLion"},
-    { fileName: "css3", name: "CSS"},
-    { fileName: "dotnetcore", name: ".NET Core"},
-    { fileName: "eclipse", name: "Eclipse"},
-    { fileName: "git", name: "Git"},
-    { fileName: "html5", name: "HTML"},
-    { fileName: "intellij", name: "IntelliJ IDEA"},
-    { fileName: "java", name: "Java"},
-    { fileName: "javascript", name: "JavaScript"},
+  skills: Array<Skills> = [
+    // Frontend Languages
+    { fileName: "html5", name: "HTML" },
+    { fileName: "css3", name: "CSS" },
+    { fileName: "javascript", name: "JavaScript" },
+    { fileName: "typescript", name: "TypeScript" },
+
+    // Frontend Frameworks
+    { fileName: "angular", name: "Angular" },
+    { fileName: "nextjs", name: "Next.js" },
+    { fileName: "react", name: "React" },
+    { fileName: "react", name: "React Native" },
+
+    // Styling Tools
+    { fileName: "bootstrap", name: "Bootstrap" },
+    { fileName: "tailwindcss", name: "Tailwind CSS" },
+
+    // Backend Development
+    { fileName: "nodejs", name: "Node.js" },
+
+    // Programming Languages
+    { fileName: "c", name: "C" },
+    { fileName: "cplusplus", name: "C++" },
+    { fileName: "java", name: "Java" },
+    { fileName: "python", name: "Python" },
+
+    // Frameworks & Libraries
+    { fileName: "spring", name: "Spring Boot" },
+
+    // Databases
     { fileName: "mysql", name: "MySQL" },
-    { fileName: "nodejs", name: "Node.js"},
-    { fileName: "postman", name: "Postman"},
-    { fileName: "python", name: "Python"},
-    { fileName: "react", name: "React"},
-    { fileName: "spring", name: "Spring Boot"},
     { fileName: "sqlite", name: "SQLite" },
-    { fileName: "tailwindcss", name: "Tailwind CSS"},
-    { fileName: "trello", name: "Trello"},
-    { fileName: "typescript", name: "TypeScript"},
-    { fileName: "vscode", name: "VS Code"}
+
+    // IDEs
+    { fileName: "androidstudio", name: "Android Studio" },
+    { fileName: "clion", name: "CLion" },
+    { fileName: "eclipse", name: "Eclipse" },
+    { fileName: "intellij", name: "IntelliJ IDEA" },
+    { fileName: "vscode", name: "VS Code" },
+
+    // Version Control
+    { fileName: "git", name: "Git" },
+
+    // API & Development Tools
+    { fileName: "postman", name: "Postman" },
+
+    // Cloud Platforms
+    { fileName: "amazonwebservices", name: "AWS" },
+
+    // Project Management
+    { fileName: "trello", name: "Trello" },
   ];
 
+
+  ngOnInit(): void {
+    // `localeCompare` returns a number indicating whether 'a' string comes 
+    // before/after/is the same as 'b' string.
+    this.skills.sort((a: any, b: any) => a.fileName.localeCompare(b.fileName));
+  }
 }
